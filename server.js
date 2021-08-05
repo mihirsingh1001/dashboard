18399ccb653f1a457b9d23696c42d581df6cdc71
Mihirsingh bais <63207149+mihirsingh1001@users.noreply.github.com>
2021-08-05 20:19:09 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/dashboard'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/dashboard/'}),
+);
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
